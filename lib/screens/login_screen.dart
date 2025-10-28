78c51a978b156b135cbd4b2d811233600e2b3bb9
Gustavo <ogustavoress@gmail.com>
2025-10-28 15:12:16 -0300

    feat: criando a estrutura do formulário de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,8 +2,34 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key});
   final _formKey = GlobalKey<FormState>();
 
-  @override
-  Widget build(BuildContext context) {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Colors.blue.shade50,
+            body: Center(
+                child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(24),
+                    child: Card(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Padding(
+                            padding: const EdgeInsets.all(24),
+                            child: Form(
+                                key: _formKey,
+                                child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                                    children: [
 
-  }
+                                    ],
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
 }
